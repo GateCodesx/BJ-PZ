003fc47a931ec693a6ee35cd84ba1b736805fbf7
GateCodesx <lsx374686571@163.com>
2021-11-04 21:13:04 +0800

    nihao

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import vue3videoPlay from "vue3-video-play"; // 引入组件
-import "vue3-video-play/dist/style.css"; // 引入css
-
-createApp(App)
-.use(vue3videoPlay)
-.mount('#app')
